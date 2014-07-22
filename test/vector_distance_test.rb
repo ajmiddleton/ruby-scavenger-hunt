5e9bf09a2af6c6da343471cf2faefa899b56cf20
Andrew Middleton <middleton.andrew.j@gmail.com>
2014-07-22 13:39:27 -0500

    failed vector + integer test

--- test/vector_distance_test.rb
+++ test/vector_distance_test.rb
@@ -3,27 +3,10 @@ require 'matrix'
 require_relative '../lib/vector_distance'
 
 class ExampleTest < MiniTest::Unit::TestCase
-  def test_vector_distance_0
-    vector_final = Vector[1,1]
-    vector_initial = VectorDistance[0,1]
-
-    distance = vector_initial.distance(vector_final)
-    assert_equal distance, 1
-  end
-
-  def test_vector_distance_1
-    vector_final = Vector[3,1]
-    vector_initial = VectorDistance[0,1]
-
-    distance = vector_initial.distance(vector_final)
-    assert_equal distance, 3
-  end
-
-  def test_vector_distance_2
-    vector_final = Vector[-3,1]
-    vector_initial = VectorDistance[0,1]
-
-    distance = vector_initial.distance(vector_final)
-    assert_equal distance, 3
+  def test_add_vector_and_integer
+    vector = Vector[1,1]
+    vector += 1
+    assert_equal vector[0], 2
+    assert_equal vector[1], 2
   end
 end
