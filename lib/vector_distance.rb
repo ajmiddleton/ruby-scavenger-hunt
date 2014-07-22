99aee98a20b02f1b5de4c91c36f33a142b6e3d14
Andrew Middleton <middleton.andrew.j@gmail.com>
2014-07-22 10:28:42 -0500

    vector distance

--- lib/vector_distance.rb
+++ lib/vector_distance.rb
@@ -1,10 +1,8 @@
 require 'matrix'
 
 class VectorDistance
-  Vector = Matrix::Vector
-
   def self.calculate(vector_final, vector_initial)
     new_vector = vector_final - vector_initial
     new_vector.magnitude
-  end  
+  end
 end
